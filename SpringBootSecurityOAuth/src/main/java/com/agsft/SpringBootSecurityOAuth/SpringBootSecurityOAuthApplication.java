ffb883afbe83cf80ad1a8f7a3627945c7b52c6e5
Shivendra Kumar <123854927+shivendra-somr@users.noreply.github.com>
2024-02-08 11:03:37 +0530

    Simple OAuth2 App (Github as 3rd party app)

---
+++ SpringBootSecurityOAuth/src/main/java/com/agsft/SpringBootSecurityOAuth/SpringBootSecurityOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.agsft.SpringBootSecurityOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityOAuthApplication.class, args);
+	}
+
+}
---
+++ SpringBootSecurityOAuth/src/test/java/com/agsft/SpringBootSecurityOAuth/SpringBootSecurityOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agsft.SpringBootSecurityOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
